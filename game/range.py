bbdbac7f3c6ad211eaa7902f10392a8ebca94993
kdm1jkm <kdm1jkmm@gmail.com>
2022-07-06 07:56:55 +0900

    range관련 함수의 구현을 list대신 Generator를 리턴하는 것으로 변경

--- game/range.py
+++ game/range.py
@@ -1,3 +1,5 @@
+from typing import Generator
+
 from game.pos import Pos
 
 
@@ -18,25 +20,18 @@ class Range:
     def size(self):
         return self.width * self.height
 
-    def all_pos(self) -> list[Pos]:
-        return [
-            Pos(x, y)
-            for y in range(self.pos1.y, self.pos2.y + 1)
-            for x in range(self.pos1.x, self.pos2.x + 1)
-        ]
+    def all_pos(self) -> Generator[Pos, None, None]:
+        for y in range(self.pos1.y, self.pos2.y + 1):
+            for x in range(self.pos1.x, self.pos2.x + 1):
+                yield Pos(x, y)
 
     def __str__(self):
         return f"{self.pos1} to {self.pos2}"
 
 
-def get_all_possible_range(_range: Range) -> list[Range]:
-    init_poses = _range.all_pos()
-    range_end = Pos(_range.width - 1, _range.height - 1)
-    return sorted(
-        [
-            Range(init_pos, end_pos)
-            for init_pos in init_poses
-            for end_pos in Range(init_pos, range_end).all_pos()
-        ],
-        key=lambda _range: _range.size,
-    )
+def get_all_possible_range(_range: Range) -> Generator[Range, None, None]:
+    for size in sorted(_range.all_pos(), key=lambda pos: (pos.x + 1) * (pos.y + 1)):
+        end_pos = Pos(_range.width - size.x - 1, _range.height - size.y - 1)
+        init_poses = Range(Pos(0, 0), end_pos).all_pos()
+        for init_pos in init_poses:
+            yield Range(init_pos, Pos(init_pos.x + size.x, init_pos.y + size.y))
